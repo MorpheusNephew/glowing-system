1295538dda720f6ee208a7e3da92c667d517c5a9
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-10 19:56:24 +0200

    Added Models project

---
+++ src/ForexTrader.Models/PriceRange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ForexTrader.Models
+{
+    public class PriceRange
+    {
+        public double High;
+
+        public double Low;
+
+        public double Open;
+
+        public double Close;
+
+        public bool UpwardTrend()
+        {
+            return Close > Open;
+        }
+
+        public bool DownwardTrend()
+        {
+            return Open > Close;
+        }
+
+        public bool TopShadow()
+        {
+            return High > Open && High > Close;
+        }
+
+        public bool BottomShadow()
+        {
+            return Low < Open && Low < Close;
+        }
+    }
+}
