6c1bc69657bf1ce4126a5ff9e38853465949c88f
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-11 15:16:13 +0200

    Added CandleStickExceptions

---
+++ src/ForexTrader.Strategies/Exceptions/CandleStickExceptions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ForexTrader.Strategies.Exceptions
+{
+    public static class CandleStickExceptions
+    {
+        public static Exception MaxNumberOfCandleSticks(Type strategyType, int numberOfCandleSticks)
+        {
+            return new OverflowException($"Max number of candle sticks for {strategyType} is {numberOfCandleSticks}");
+        }
+    }
+}
