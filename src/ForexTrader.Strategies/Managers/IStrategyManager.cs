8b0d615421494509cc2c557367dff7e369ab6751
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-11 12:04:34 +0200

    Only focusing on adding strategies

--- src/ForexTrader.Strategies/Managers/IStrategyManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ForexTrader.Strategies.Managers
-{
-    public interface IStrategyManager
-    {
-        public IList<IStrategy> GetStrategies();
-    }
-}
