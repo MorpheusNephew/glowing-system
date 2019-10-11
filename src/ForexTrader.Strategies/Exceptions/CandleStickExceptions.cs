cadf8eca491ef42cb28329925c5546fb23fb6fb0
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-11 17:42:26 +0200

    Changed to expression body

--- src/ForexTrader.Strategies/Exceptions/CandleStickExceptions.cs
+++ src/ForexTrader.Strategies/Exceptions/CandleStickExceptions.cs
@@ -4,9 +4,6 @@ namespace ForexTrader.Strategies.Exceptions
 {
     public static class CandleStickExceptions
     {
-        public static Exception MaxNumberOfCandleSticks(Type strategyType, int numberOfCandleSticks)
-        {
-            return new OverflowException($"Max number of candle sticks for {strategyType} is {numberOfCandleSticks}");
-        }
+        public static Exception MaxNumberOfCandleSticks(Type strategyType, int numberOfCandleSticks) => new OverflowException($"Max number of candle sticks for {strategyType} is {numberOfCandleSticks}");
     }
 }
