67cb4be21f800ca59493b9fb3827f9cfb229782c
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-11 10:17:52 +0200

    Added candle stick shape to be able to easily map patterns

---
+++ src/ForexTrader.Models/CandleStickShape.cs
@@ -0,0 +1,17 @@
+namespace ForexTrader.Models
+{
+    public enum CandleStickShape
+    {
+        LongUpwardTrend,
+        LongDownwardTrend,
+        LongUpperShadow,
+        LongLowerShadow,
+        SpinningTop,
+        FourPriceDoji,
+        GravestoneDoji,
+        DragonflyDoji,
+        LongLeggedDoji,
+        ShortUpwardTrend,
+        ShortDownwardTrend
+    }
+}
