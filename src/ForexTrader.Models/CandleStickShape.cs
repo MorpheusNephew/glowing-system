65699e51c216dea08dc213f700089622574368b5
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-11 11:17:20 +0200

    Added more readable candle stick shapes

--- src/ForexTrader.Models/CandleStickShape.cs
+++ src/ForexTrader.Models/CandleStickShape.cs
@@ -2,16 +2,25 @@ namespace ForexTrader.Models
 {
     public enum CandleStickShape
     {
-        LongUpwardTrend,
-        LongDownwardTrend,
-        LongUpperShadow,
-        LongLowerShadow,
-        SpinningTop,
-        FourPriceDoji,
-        GravestoneDoji,
-        DragonflyDoji,
-        LongLeggedDoji,
-        ShortUpwardTrend,
-        ShortDownwardTrend
+        LongBodyLongLowerShadow,
+        LongBodyLongerLowerShadow, // Longer than the upper shadow
+        LongBodyLongerUpperShadow, // Longer than the lower shadow
+        LongBodyLongShadows,
+        LongBodyLongUpperShadow,
+        LongBodyNoShadows,
+
+        ShortBodyLongLowerShadow,
+        ShortBodyLongerLowerShadow, // Longer than the upper shadow
+        ShortBodyLongerUpperShadow, // Longer than the lower shadow
+        ShortBodyLongShadows,
+        ShortBodyLongUpperShadow,
+        ShortBodyNoShadow,
+
+        TinyBodyLongLowerShadow,
+        TinyBodyLongerLowerShadow, // Longer than the upper shadow
+        TinyBodyLongerUpperShadow, // Longer than the lower shadow
+        TinyBodyLongShadows,
+        TinyBodyLongUpperShadow,
+        TinyBodyNoShadows
     }
 }
