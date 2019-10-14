a36a9a5af6557583e6c0ca7ad76cf886298fe0a5
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-14 13:41:57 +0200

    Created factory method to get the shape of the candle stick

---
+++ src/ForexTrader.Models/Factories/CandleStickShapeFactory.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ForexTrader.Models.Factories
+{
+    public static class CandleStickShapeFactory
+    {
+        public static CandleStickShape GetCandleStickShape(PriceRange priceRange)
+        {
+            // TODO: Fill in method to get candle stick shape
+            throw new NotImplementedException();
+        }
+    }
+}
