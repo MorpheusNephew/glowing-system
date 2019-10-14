89ba45ba206513db8c842193ba76c06d3a7b872b
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-14 17:39:04 +0100

    Removed unnecessary tests and extensions

--- src/ForexTrader.Models/Extensions/CandleStickShapeExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ForexTrader.Models.Extensions
-{
-    public static class CandleStickShapeExtensions
-    {
-        public static string ToString(this CandleStickShape value) => Enum.GetName(typeof(CandleStickShape), value);
-    }
-}
--- src/ForexTrader.Models/Extensions/TrendExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ForexTrader.Models.Extensions
-{
-    public static class TrendExtensions
-    {
-        public static string ToString(this Trend value) => Enum.GetName(typeof(Trend), value);
-    }
-}
