b35dc74f3f65ef09543f6c6e121027972b738243
Jelani Jackson <jelani.t.jackson@gmail.com>
2019-10-17 18:07:28 +0100

    Added project for running tests

---
+++ src/ForexTrader.ConsoleRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForexTrader.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there, this is some amazing stuff going down");
+        }
+    }
+}
